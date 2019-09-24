c520ed938f288868ee9fc79cc1b99fff346a4bab
Steven Dismuke <stevelp99@yahoo.com>
2019-09-24 09:21:00 -0500

    blackjack

---
+++ 05-week/0-monday/labs/Steven-Dismuke/Blackjack-DC/blackjack.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("deal-button").addEventListener("deal-button", dealCards);
+function dealCards(){
+    document.getElementById("dealer-hand").innerHTML='<img src="images/king_of_spades.png" alt=cards><img src="images/king_of_clubs.png" alt=cards>'
+};
+
+document.getElementById("deal-button").addEventListener("deal-button", dealCards);
+function dealCards(){
+    document.getElementById("player-hand").innerHTML='<img src="images/king_of_spades.png" alt=cards><img src="images/king_of_clubs.png" alt=cards>'
+};
